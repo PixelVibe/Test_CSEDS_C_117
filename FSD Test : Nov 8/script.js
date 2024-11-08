d9951ff9234ce8ce0769bfe3ea3f6549be6a39e3
Parth Bhardwaj <69952086+PixelVibe@users.noreply.github.com>
2024-11-08 14:14:54 +0530

    Update script.js

--- FSD Test : Nov 8/script.js
+++ FSD Test : Nov 8/script.js
@@ -4,3 +4,10 @@ document.getElementById("quantity").addEventListener("input", function () {
     const totalPrice = price * quantity;
     document.querySelector(".total-price").textContent = `Total Price: Rs. ${totalPrice}`;
 });
+
+document.querySelector(".buy-now-button").addEventListener("click", function () {
+    const quantity = parseInt(document.getElementById("quantity").value) || 1;
+    const price = 49000;
+    const totalPrice = price * quantity;
+    alert(`${quantity} items worth Rs. ${totalPrice} have been added to your cart.`);
+});
